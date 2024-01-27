302cdea3e8b1479c9431bed1987bbd41d847e819
Mxroz <supermanformedia@gmail.com>
2024-01-27 06:51:20 +0300

    two commit

--- testDRF/testapi/models.py
+++ testDRF/testapi/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Anime(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
+    desc = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
